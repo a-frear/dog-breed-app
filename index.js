6384961deaff6b0b1eca9d6a4b430d5de75ae667
a_frear <amy.frear@gmail.com>
2020-10-20 21:03:20 -0400

    Update js

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ function watchForm() {
     });
 };
 
-function getDogImage() { 
-  const breed = $('.user-breed').val();
-  console.log(breed);
-  const url = `https://dog.ceo/api/breed/${breed}/images/random`;
-  console.log(url);
+function getDogImage() {
+    const breed = $('.user-breed').val();
+    console.log(breed);
+    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+    console.log(url);
     fetch(url)
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('We don\'t have that breed on file!'));
+        .then(response => response.json())
+        .then(responseJson =>
+            displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later!'));
 }
 
 
 function displayResults(responseJson) {
     console.log(responseJson);
-       $('.results-img').replaceWith(
-            `<img src="${responseJson.message}" class="results-img">`);
+    $('.results-img').replaceWith(
+        `<img src="${responseJson.message}" class="results-img">`);
     $('#hidden-section').removeClass('hidden');
     if (responseJson.status === 'error') {
-        alert('Dog Breed is not found');
+        alert('We don\'t have that breed right now!');
         $('#hidden-section').addClass('hidden');
     }
 };
 
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
 });
